0bc7488267da062f09f24ed7c2316f41af471f6d
Tamara Mitrovska <tamaram@mit.edu>
2020-01-17 13:46:19 +0530

    Update login

--- frontend/web/src/components/home/home.js
+++ frontend/web/src/components/home/home.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from "react";
 import Footer from "./Footer/footer";
 import Header from "./Header/header";
+import LoginForm from "./login-signup/LoginForm";
 
 const Home = () => {
   return (
     <Fragment>
       <Header />
+      <LoginForm/>
       <Footer />
     </Fragment>
   );
--- frontend/web/src/index.js
+++ frontend/web/src/index.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
