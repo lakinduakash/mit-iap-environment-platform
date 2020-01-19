9e270824eb2a9e0b789e9811337dc6aa2ce3c7e8
shikharjag <shikhar00j@gmail.com>
2020-01-19 20:30:35 +0530

    update app.js

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -11,3 +11,21 @@ function App() {
 }
 
 export default App;
+
+
+
+// const position = [51.505, -0.09]
+// const map = (
+//   <Map center={position} zoom={13}>
+//     <TileLayer
+//       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//       attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+//     />
+//     <Marker position={position}>
+//       <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
+//     </Marker>
+//   </Map>
+// )
+
+// render(map, document.getElementById('map-container'))
+// export default App;
