4a079ff5dc1f13a1557d39d2bfaa4f934613bd32
shikharjag <shikhar00j@gmail.com>
2020-01-21 14:51:59 +0530

    added backend work

--- frontend/web/src/Form.js
+++ frontend/web/src/Form.js
@@ -2,9 +2,12 @@ import React from "react"
 import BMap from './Map'
 import { Map, CircleMarker, TileLayer, Polygon } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import axios from 'axios'; 
+
 import { polygon } from 'leaflet';
 
 var yala = [[6.279629, 81.424341], [6.668179, 81.771591], [6.713189,81.716037], [6.610547,81.587634], [6.607818, 81.529269], [6.618731, 81.510730], [6.607136, 81.494937], [6.702619, 81.354861], [6.624188, 81.266284], [6.536876, 81.263538], [6.510270, 81.135135], [6.396327, 81.145993], [6.351289, 81.199551], [6.386092, 81.273022], [6.461831, 81.276455], [6.447503, 81.335507], [6.322626, 81.383572], [6.309318, 81.374131], [6.300446, 81.372929], [6.277241, 81.405201]] 
+
 
 
 class Form extends React.Component {
@@ -15,7 +18,17 @@ class Form extends React.Component {
     points: [[10.022443, 80.260258],[5.815325, 80.692459]],
     coords: this.props.coords
   }
+  axios.get('http://localhost:9090/add/createPoly', this.state)
+            .then(function(response){
+                console.log(yala)
+                console.log("holla")
+                yala = response
+                console.log(yala);
+            })
+  
 }
+
+
 
   
 handleChange = (e) => {
@@ -58,6 +71,7 @@ handleSubmit = (e) => {
   e.preventDefault() 
 }
 render() {
+  
     let {cats} = this.state
     let error_msg;
     let poly;
@@ -68,8 +82,10 @@ render() {
       poly = <Polygon  positions = {this.state.points}/>
       error_msg=""
     }
+    //console.log("yala", yala)
     return (
       <div>
+        {yala.length}
         <Map
           style={{ height: "480px", width: "60%" }}
 
@@ -111,52 +127,18 @@ render() {
                   value={cats[idx].long} 
                   className="long"
                 />
-              </div> 
+                
+              </div>
+              
             )
+            
           })
         }
         <div>
-          {error_msg}
-        </div>
-        <input type="submit" value="Check Availability" /> 
+              {error_msg}
+              </div>
 
-        <br></br>
-        <label>
-          <br></br>
-            Land Type: 
-            <select>
-              <option value="Plantation">Plantation</option>
-              <option value="Forestation">Forestation</option>
-              <option selected value="Deforestation">Deforestation</option>
-              <option value="Hotel">Hotel</option>
-            </select>
-        </label>
-
-        <br></br>
-        <label>
-            Timeframe: 
-            <input
-            Timeframe=""
-            type="text"
-            value={this.state.fullname}
-            onChange={this.handleInputChange} />
-        </label>
-
-        <br></br>
-        <label>
-            Description: 
-            <input
-            Timeframe=""
-            type="text"
-            value={this.state.fullname}
-            onChange={this.handleInputChange} />
-        </label>
-        <br></br>
-        <label>Attach Files: </label>
-        <input type="file" />
-        <br></br>
-        <input type="submit" value="Submit Form" /> 
-        
+        <input type="submit" value="Submit" /> 
       </form>
       </div>
       
