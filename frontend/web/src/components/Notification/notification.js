1ccd6a7c5bdf62f1254d4a19e7f107e86fd8c836
TharushiJay <tharushi68@gmail.com>
2020-01-22 11:04:32 +0530

    Display notification

--- frontend/web/src/components/Notification/notification.js
+++ frontend/web/src/components/Notification/notification.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 class Notification extends Component {
 
   // instance of websocket connection as a class property
   ws = new WebSocket('ws://localhost:9095')
+  msg = "";
 
   componentDidMount() {
       this.ws.onopen = () => {
@@ -14,6 +21,7 @@ class Notification extends Component {
       this.ws.onmessage = evt => {
       // listen to data sent from the websocket server
       const message = evt.data
+      msg = message
       this.setState({dataFromServer: message})
       alert('Notification received : ' + message);
       console.log(message)
@@ -28,7 +36,11 @@ class Notification extends Component {
   }
 
   render(){
-      return <ChildComponent websocket={this.ws} />
+    return (<div>
+       <ChildComponent websocket={this.ws} />
+      < Notification_holder value = {this.msg} />
+      </div>
+    )
   }
 }
 
@@ -73,4 +85,52 @@ class ChildComponent extends Component {
       </form>
     );
   }
+}
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+class Notification_holder extends Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+  }
+
+    render() {
+      return (
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              
+            </Typography>
+            <Typography variant="h5" component="h2">
+              Notification
+            </Typography>
+            <Typography className={classes.pos} color="textSecondary">
+            </Typography>
+            <Typography variant="body2" component="p">
+              {this.props.value}
+              <br />
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+      );
+    }
 }
