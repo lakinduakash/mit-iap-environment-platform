02200fb9d56b92aec092ef306d03606c4bff08f1
TharushiJay <tharushi68@gmail.com>
2020-01-23 11:40:04 +0530

    Show notifications in alert

--- frontend/web/src/components/Notification/notification.js
+++ frontend/web/src/components/Notification/notification.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import Alert from '@material-ui/lab/Alert';
 
 class Notification extends Component {
 
@@ -86,52 +82,23 @@ class ChildComponent extends Component {
     );
   }
 }
-
-const classes = makeStyles({
-  card: {
-    minWidth: 275,
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+    '& > * + *': {
+      marginTop: theme.spacing(2),
+    },
   },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
+}));
 
-class Notification_holder extends Component{
+function Notification_holder(notif){
+  const classes = useStyles();
 
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
-  }
-
-    render() {
       return (
-        <Card className={classes.card}>
-          <CardContent>
-            <Typography className={classes.title} color="textSecondary" gutterBottom>
-              
-            </Typography>
-            <Typography variant="h5" component="h2">
-              Notification
-            </Typography>
-            <Typography className={classes.pos} color="textSecondary">
-            </Typography>
-            <Typography variant="body2" component="p">
-              {this.props.value}
-              <br />
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Learn More</Button>
-          </CardActions>
-        </Card>
+        <div className={classes.root}>
+        <Alert variant="outlined" severity="info">
+        {notif.value}
+        </Alert>
+        </div>
       );
-    }
 }
