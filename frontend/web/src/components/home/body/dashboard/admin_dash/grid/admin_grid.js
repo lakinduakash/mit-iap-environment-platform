7fb81ed60d15ec8dbe6c751d2f8df51b05b0892e
yashodgayashan <yashodgayashan@gmail.com>
2020-01-23 18:04:18 +0530

    Completed admin table.

--- frontend/web/src/components/home/body/dashboard/admin_dash/grid/admin_grid.js
+++ frontend/web/src/components/home/body/dashboard/admin_dash/grid/admin_grid.js
@@ -1,46 +1,85 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./admin_grid.css";
 import { useHistory } from "react-router-dom";
 import { Button } from "@material-ui/core";
+import axios from "axios";
 
 const Admin_grid = () => {
   let history = useHistory();
+  const [loading] = useState(null);
+  const [requests, setRequests] = useState();
+
+  useEffect(() => {
+    axios
+      .get("http://0.0.0.0:9070/admin-services/get-all-requests")
+      .then(function(response) {
+        setRequests(response.data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }, [loading]);
+
   const Table = () => {
     return (
       <div>
         <table className="table table-hover table-bordered custom-table">
           <thead>
             <tr>
-              <th>Requset Number</th>
               <th>Request Title</th>
               <th>Request Description</th>
               <th>Owner</th>
               <th>Status</th>
-              <th>Tags</th>
-              <th>Change Tags</th>
               <th>View</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td>
-                <Button
-                  type="button"
-                  onClick={() => {
-                    history.push("/admin-dash/request");
-                  }}
-                >
-                  View
-                </Button>
-              </td>
-            </tr>
+            {requests != null ? (
+              requests.map(request => (
+                <tr key={request.requsetNumber}>
+                  <td>{request.requestTitle}</td>
+                  <td>
+                    <ul>
+                      {JSON.parse(request.requestBody).timeframe != "" ? (
+                        <li>
+                          Duration - {JSON.parse(request.requestBody).timeframe}
+                        </li>
+                      ) : (
+                        <p></p>
+                      )}
+                      {JSON.parse(request.requestBody).description != "" ? (
+                        <li>
+                          Description -{" "}
+                          {JSON.parse(request.requestBody).description}
+                        </li>
+                      ) : (
+                        <p></p>
+                      )}
+                    </ul>
+                  </td>
+                  <td>{request.owner}</td>
+                  <td>
+                    {request.status != "" ? (
+                      <p>request.status</p>
+                    ) : (
+                      <p>Pending</p>
+                    )}
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-info"
+                      onClick={() => {
+                        history.push("/admin-dash/request");
+                      }}
+                    >
+                      View
+                    </button>
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr></tr>
+            )}
           </tbody>
         </table>
       </div>
